96df7d98e9dcf53caef35712f3d1febb2d9a2dae
MarcusML <114195461+m4rcusml@users.noreply.github.com>
2025-02-11 14:12:09 -0300

    fixed

--- first_ponderada.js
+++ first_ponderada.js
@@ -24,16 +24,16 @@ console.log(`Média final: ${media.toFixed(3)}`, '\n');
 // dar npm install readline-sync
 
 const prompt = require('readline-sync');
-let media2 = 0, i = 1;
+let media2 = 0, i = 0;
 let quantNotas = prompt.questionInt('Quantas notas voce deseja inserir? ');
 
-while (i <= quantNotas) {
+while (i < quantNotas) {
   i++;
 
-  media2 += prompt.questionFloat(`Digite a nota ${i - 1}: `);
+  media2 += prompt.questionFloat(`Digite a nota ${i}: `);
 }
 
-media2 = media2 / (i - 1);
+media2 = media2 / i;
 
 if (media2 >= 7) {
   console.log('APROVADO');
